f59308c7893b332c69d0b6f6b303fd1ffd965ba6
Sapthaka Godage <53206450+Sapthaka2346@users.noreply.github.com>
2020-05-05 22:49:42 +0530

    Changes in java class.

--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -224,44 +224,5 @@ public class Hospital {
 		return output;
 	}
 	
-
-
-	public String userLogin(String username, String password) {
-		String output = "";
-		try {
-			Connection con = connect();
-			if (con == null) {
-				return "Error while connecting to the database for reading.";
-			}
-			
-			String query ="select username,phoneNo from users where username="+username+" AND phoneNo="+password;
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			ResultSet rs = ((java.sql.Statement) preparedStmt).executeQuery(query);
-			
-			
-			  while (rs.next()) {
-			        String UserName = rs.getString("username");
-			        String Password = rs.getString("phoneNo");
-			        
-			  
-			        if((username.equalsIgnoreCase(UserName)) && (password.equalsIgnoreCase(Password))) {
-			        	//output ="     Login Failed  !!";
-			        	output ="     Login Successful  !!           You're logged as"   +username;
-			        	}
-		              else {
-		                output ="      Login Failed...!!";
-		            	 //output ="     Login Successful  !!           You're logged as"   +username;
-		              	} 
-			  	}
-			
-			con.close();
-			
-		}catch (Exception e) {
-			output = "Error while Logging.";
-			System.err.println(e.getMessage());
-		}
-		return output;
-	}
-	
 	
 }
